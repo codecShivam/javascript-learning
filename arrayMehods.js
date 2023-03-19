1e9a22be38d442b0e7c8d66e99ebc5b953ee346e
Shivam <sy654906@gmail.com>
2023-03-19 08:22:16 +0530

    includes()

--- arrayMehods.js
+++ arrayMehods.js
@@ -1,25 +1,38 @@
-//isarray()
+// //isarray()
+// var a = ["Shivam", "Abdul", "Rishabh", "Aditya"];
+// document.write(a + "<br> <br>");
+
+// var b = Array.isArray(a);
+// document.write(b + "<br> <br>");
+
+// var c = 10;
+// b = Array.isArray(c);
+// document.write(b + "<br> <br>");
+
+// var d = [[10, 23, 44], ["hi", "by"]];
+// b = Array.isArray(d);
+// document.write(b + "<br> <br>");
+
+// let z = [];
+// var n = prompt("Enter index of the array!");
+// for (let i = 0; i < n; i++) {
+//     z[i] = prompt("Insert values in the array "); //isArray() is used to check whether the entered array is array or not
+// }
+// if (Array.isArray(z)) {
+//     document.write("You have entered the correct input!!");
+// } else {
+//     document.write("You have entered the wrong input!");
+// }
+
+//******include()******** */
 var a = ["Shivam", "Abdul", "Rishabh", "Aditya"];
 document.write(a + "<br> <br>");
 
-var b = Array.isArray(a);
-document.write(b + "<br> <br>");
+var b= a.includes("Shivam");
+document.write(b + "<br> <br>");//true
 
-var c = 10;
-b = Array.isArray(c);
-document.write(b + "<br> <br>");
+b= a.includes("Neha");
+document.write(b + "<br> <br>");//false
 
-var d = [[10, 23, 44], ["hi", "by"]];
-b = Array.isArray(d);
-document.write(b + "<br> <br>");
-
-let z = [];
-var n = prompt("Enter index of the array!");
-for (let i = 0; i < n; i++) {
-    z[i] = prompt("Insert values in the array "); //isArray() is used to check whether the entered array is array or not
-}
-if (Array.isArray(z)) {
-    document.write("You have entered the correct input!!");
-} else {
-    document.write("You have entered the wrong input!");
-}
+b = a.includes("aditya");//case sensitive
+document.write(b + "<br> <br>");//false
