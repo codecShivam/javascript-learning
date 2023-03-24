9ecd1ab995f30b5471c8af7335a77efc559749d4
Shivam <sy654906@gmail.com>
2023-03-24 19:13:29 +0530

    map() function

--- app.js
+++ app.js
@@ -106,14 +106,38 @@
 // mystey;
 
 
-var obj = {
-    firstName: "Shivam",
-    lastName : "Yadav",
-    Age : 25,
-    email: "sy654906@gamil.com",
-};
+// var obj = {
+//     firstName: "Shivam",
+//     lastName : "Yadav",
+//     Age : 25,
+//     email: "sy654906@gamil.com",
+// };
+// //it will work as the for loop for each key and and each value of the object
+// for (var key in obj){
+//     document.write(key + " : " +  obj[key] + "<br>");
+//     document.write(`${key} : ${obj[key]} <br>`);
+// }
 
-for (var key in obj){
-    document.write(key + " : " +  obj[key] + "<br>");
-    document.write(`${key} : ${obj[key]} <br>`);
+
+//*****************Map()******************************it will create a new array 
+var ary =[11,4,9,16];
+
+var b=ary.map(test);
+document.write(b);
+
+function test(x){
+    return x*10;
+}
+
+
+var ary = [
+    {fname: "Shivam", lname: "Yadav"},
+    {fname: "Rishabh", lname: "Gupta"},
+    {fname: "Abdul", lname: "Samad"}
+]
+var b=ary.map(test);
+document.write(b);
+
+function test(x){
+    return " " + x.fname + " " + x.lname + " ";
 }
