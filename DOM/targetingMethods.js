ef0b2ba394d86369fa603298f2224a19f538de19
Shivam <sy654906@gmail.com>
2023-03-26 15:58:23 +0530

    DOM targetingMethods

---
+++ DOM/targetingMethods.js
@@ -0,0 +1,15 @@
+var element;
+
+element = document.links[0];
+element = document.images;
+element =document.fonts;
+element = document.doctype;
+element = document.URL;
+element = document.domain;
+element = document.baseURI;
+
+element = document.getElementById("header");
+element= document.getElementsByClassName("list[0]")
+element = document.getElementsByTagName("ul")[1]
+
+console.log(element);
