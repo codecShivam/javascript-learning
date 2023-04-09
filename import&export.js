76302b9f0b9038bf228eaa496754d2cb47e949b4
codecShivam <codecshivam@gmail.com>
2023-04-09 23:02:44 +0530

    import and export

---
+++ import&export.js
@@ -0,0 +1,9 @@
+// import { capitalizeString } from "./string_function.js";
+// const cap = capitalizeString("hello!");
+// console.log(cap);
+
+
+import   capitalizeString  from "./string_function.js"
+
+const cap = capitalizeString("fjdofjo");
+console.log(cap);
---
+++ string_function.js
@@ -0,0 +1,12 @@
+// // export const capitalizeString = str => str.toUpperCase()
+
+
+
+// const capitalizeString = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+
+// export const foo= "bar";
+// export const bar = "foo";
+// export  default capitalizeString ;
+
